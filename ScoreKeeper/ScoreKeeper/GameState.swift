c5f1301c6ec09c83959545dd68bf3ef73e007b23
divyadeepmishra <divydeepmishra@gmail.com>
2025-06-19 08:51:06 +0530

    Adding ScoreKeeper

---
+++ ScoreKeeper/ScoreKeeper/GameState.swift
@@ -0,0 +1,14 @@
+//
+//  GameState.swift
+//  ScoreKeeper
+//
+//  Created by DIVYADEEP MISHRA on 14/06/25.
+//
+
+import Foundation
+
+enum GameState {
+    case setup
+    case playing
+    case gameOver
+}
---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by DIVYADEEP MISHRA on 14/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
