8606755cb3e044cb010bf1043140feea74da6975
divyadeepmishra <divydeepmishra@gmail.com>
2025-07-14 20:19:41 +0530

    A project based on creating a ML model on Linear REgression and using it to build an app

---
+++ TrailAnalyzer/TrailAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailAnalyzerApp.swift
+//  TrailAnalyzer
+//
+//  Created by DIVYADEEP MISHRA on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TrailAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
